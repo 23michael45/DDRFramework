0c248c046b6bc8658ef6d470891a4bafb56fdb08
23michael45 <michael2345@live.cn>
2018-12-06 21:36:04 +0800

    add all shared file

---
+++ proto/compile_proto.bat
@@ -0,0 +1,5 @@
+protoc BaseCmd.proto --cpp_out=./
+protoc BaseCmd.proto --java_out=./
+protoc BaseCmd.proto --csharp_out=./
+rem protoc BaseCmd.proto --js_out=./
+rem protoc BaseCmd.proto --python_out=./
