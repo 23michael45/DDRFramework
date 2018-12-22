428f0818b4603f385e353d4fbec0e826bc4e17a0
23michael45 <michael2345@live.cn>
2018-12-22 18:11:50 +0800

    change proto stream local remote seperate

--- tools/xlsx2xml/Xlsx2Xml.bat
+++ tools/xlsx2xml/Xlsx2Xml.bat
@@ -3,3 +3,6 @@ Xlsx2Xml.exe xlsx/ xml/
 echo The current directory is %CD%
 set CurrentPatrh=%CD%
 xcopy /Y %CurrentPatrh%\xml\*.* %CurrentPatrh%\..\..\..\DDRLocalServer\DDR_LocalServer\Config /s /i
+
+xcopy /Y %CurrentPatrh%\xml\StreamRelayService\*.* %CurrentPatrh%\..\..\..\DDRStreamRelayService\DDRStreamRelayService\Config\StreamRelayService /s /i
+xcopy /Y %CurrentPatrh%\xml\Localization.* %CurrentPatrh%\..\..\..\DDRStreamRelayService\DDRStreamRelayService\Config /s /i
