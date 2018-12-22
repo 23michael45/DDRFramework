d552767e3e63479127ac8476d16a5ca99a4cca9b
23michael45 <michael2345@live.cn>
2018-12-22 18:19:16 +0800

    auto copy xml to client

--- tools/xlsx2xml/Xlsx2Xml.bat
+++ tools/xlsx2xml/Xlsx2Xml.bat
@@ -4,5 +4,9 @@ echo The current directory is %CD%
 set CurrentPatrh=%CD%
 xcopy /Y %CurrentPatrh%\xml\*.* %CurrentPatrh%\..\..\..\DDRLocalServer\DDR_LocalServer\Config /s /i
 
+
+xcopy /Y %CurrentPatrh%\xml\Client\*.* %CurrentPatrh%\..\..\..\DDRLocalServer\DDR_LocalClient\Config\Client /s /i
+xcopy /Y %CurrentPatrh%\xml\Localization.* %CurrentPatrh%\..\..\..\DDRLocalServer\DDR_LocalClient\Config /s /i
+
 xcopy /Y %CurrentPatrh%\xml\StreamRelayService\*.* %CurrentPatrh%\..\..\..\DDRStreamRelayService\DDRStreamRelayService\Config\StreamRelayService /s /i
 xcopy /Y %CurrentPatrh%\xml\Localization.* %CurrentPatrh%\..\..\..\DDRStreamRelayService\DDRStreamRelayService\Config /s /i
