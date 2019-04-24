09414da710713264e0ebc771959f53b0d82b926e
23michael45 <michael2345@live.cn>
2019-04-24 17:06:55 +0800

    add proto h

--- shell/gen_header_include.bat
+++ shell/gen_header_include.bat
@@ -3,9 +3,12 @@ echo The current directory is %~dp0
 set CurrentPath=%~dp0
 cd %CurrentPath%
 
+rmdir /s /q %CurrentPath%\..\include\Shared\
+mkdir  %CurrentPath%\..\include\Shared\
 
 
 xcopy /Y %CurrentPath%\..\src\*.h %CurrentPath%\..\include\Shared\src /s /i
+xcopy /Y %CurrentPath%\..\proto\*.h %CurrentPath%\..\include\Shared\proto /s /i
 xcopy /Y %CurrentPath%\..\thirdparty\*.h %CurrentPath%\..\include\Shared\thirdparty /s /i
 
 
