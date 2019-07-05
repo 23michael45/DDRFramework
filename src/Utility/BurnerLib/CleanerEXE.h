32de07dc56e416ad2a37daa1335863b893f2ab8e
Jerry <longjr@dadaoii.com>
2019-07-05 13:20:18 +0800

    ~ Buner and cleaner executable files converted to source files

---
+++ src/Utility/BurnerLib/CleanerEXE.h
@@ -0,0 +1,13 @@
+#ifndef __DDR_CHUNKRESOURCE_UGLY2LOOKAT_CleanerEXE_H_INCLUDED__
+#define __DDR_CHUNKRESOURCE_UGLY2LOOKAT_CleanerEXE_H_INCLUDED__
+
+namespace DDRGadgets {
+
+extern int gRES_CleanerEXE_len;
+extern int gRES_CleanerEXE_ExtraLen;
+extern char gRES_CleanerEXE_EData[];
+
+}
+
+#endif //__DDR_CHUNKRESOURCE_UGLY2LOOKAT_CleanerEXE_H_INCLUDED__
+
